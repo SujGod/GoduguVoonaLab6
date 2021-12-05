93f4204cf1d21a6ea1629e73f39eb20132929ddd
SujGod <64814324+SujGod@users.noreply.github.com>
2021-12-05 13:23:36 -0500

    Added Key Info to Hud
    Added Key Info to Hud

--- Assets/Scripts/HeadsUpDisplay.cs
+++ Assets/Scripts/HeadsUpDisplay.cs
@@ -8,6 +8,7 @@ public class HeadsUpDisplay : MonoBehaviour
     public bool isFiring;
     public int ammo;
     public int health;
+    public bool key;
 
     public float maxHealth;
     public float maxAmmo;
@@ -15,12 +16,16 @@ public class HeadsUpDisplay : MonoBehaviour
     private Character player;
     public Text ammoDisplay;
     public Text healthDisplay;
+    public Text keyDisplay;
 
     public GameObject ammoBarUI;
     public Slider ammoBarSlider;
 
     public GameObject characterhealthBarUI;
     public Slider characterHealthSlider;
+
+    public GameObject keyBarUI;
+    public Slider keySlider;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,9 +36,19 @@ public class HeadsUpDisplay : MonoBehaviour
         player = GetComponentInParent<Character>();
         ammo = player.ammo;
         health = player.health;
+        key = player.keyObtained;
 
         maxHealth = player.maxHealth;
         maxAmmo = player.maxAmmo;
+
+        if (key)
+        {
+            keySlider.value = 1;
+        }
+        else
+        {
+            keySlider.value = 0;
+        }
 
         ammoBarSlider.value = ammo / maxAmmo;
         characterHealthSlider.value = health / maxHealth;
@@ -48,6 +63,17 @@ public class HeadsUpDisplay : MonoBehaviour
         characterHealthSlider.value = health / maxHealth;
         ammoBarSlider.value = ammo / maxAmmo;
 
+        if (key)
+        {
+            keyDisplay.text = "Yes";
+            keySlider.value = 1;
+        }
+        else
+        {
+            keyDisplay.text = "No";
+            keySlider.value = 0;
+        }
+
         if (ammo != player.ammo)
         {
             ammo = player.ammo;
@@ -56,6 +82,11 @@ public class HeadsUpDisplay : MonoBehaviour
         if (health != player.health)
         {
             health = player.health;
+        }
+
+        if (key != player.keyObtained)
+        {
+            key = player.keyObtained;
         }
 
     }
