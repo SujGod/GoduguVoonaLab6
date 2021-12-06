941108107b4e1daa006dec60918ea994c98324aa
SujGod <64814324+SujGod@users.noreply.github.com>
2021-12-06 11:33:53 -0500

    Took Out Supreeth's Predator Code

--- Assets/Scripts/Predator.cs
+++ Assets/Scripts/Predator.cs
@@ -1,101 +1,3 @@
-/*using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Predator : MonoBehaviour
-{
-    [SerializeField] private float speed;
-    private float oldSpeed;
-    private bool isSpeedIncreased;
-    [SerializeField] private float rot_speed;
-    private Vector3 direction;
-    public FieldOfView fieldOfView;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        isSpeedIncreased = false;
-        oldSpeed = speed;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (direction == Vector3.zero)
-        {
-            direction = fieldOfView.GetNewDirection();
-        }
-
-        if (fieldOfView.IsPreyInVision())
-        {
-            ChasePrey(fieldOfView.WhichPrey());
-        } 
-        else
-        {
-            speed = oldSpeed;
-            isSpeedIncreased = false;
-            Wander();
-        }
-
-        Orient();
-
-    }
-
-    void Wander()
-    {
-        if (fieldOfView.AreAnyMiddleRaysHit())
-        {
-            if (fieldOfView.AreAllRaysHit())
-            {
-                RotateDirection();
-            }
-            else
-            {
-                direction = fieldOfView.GetNewDirection();
-            }
-
-        }
-        transform.position += direction * speed * Time.deltaTime;
-    }
-
-    void Orient()
-    {
-        Quaternion rot_target = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot_target, rot_speed * Time.deltaTime);
-    }
-
-    void RotateDirection()
-    {
-        direction = transform.right;
-    }
-    
-    private void ChasePrey(Collider other)
-    {
-        if (!isSpeedIncreased)
-        {
-            speed += 2;
-            isSpeedIncreased = true;
-        }
-
-        direction = other.transform.position - transform.position;
-        transform.position = Vector3.MoveTowards(transform.position, other.transform.position, speed * Time.deltaTime);
-            
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.tag == "Prey")
-        {
-            Destroy(collision.collider.gameObject);
-            speed = oldSpeed;
-            isSpeedIncreased = false;
-        }
-    }
-}
-*/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -192,9 +94,9 @@ public class Predator : MonoBehaviour
         }
 
         //if collide with a prey destroy the prey object and remove it from the list
-        if (collision.gameObject.tag == "Character")
+/*        if (collision.gameObject.tag == "Character")
         {
-            /* if (collision.gameObject != null)
+            *//* if (collision.gameObject != null)
             {
                 Destroy(collision.gameObject);
                 for (int i = 0; i < listOfPrey.Count; i++)
@@ -205,7 +107,7 @@ public class Predator : MonoBehaviour
                         listOfPrey.Remove(listOfPrey[i]);
                     }
                 }
-            }*/
-        }
+            }*//*
+        }*/
     }
 }
